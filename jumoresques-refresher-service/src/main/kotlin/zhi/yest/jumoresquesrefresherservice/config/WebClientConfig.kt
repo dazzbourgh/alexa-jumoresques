fc2be6bd29c9182adcd5b7e438906885b78db5f5
Leonid Borisevich <lborisevich@stubhub.com>
2020-03-13 22:54:08 -0700

    added request logging

--- jumoresques-refresher-service/src/main/kotlin/zhi/yest/jumoresquesrefresherservice/config/WebClientConfig.kt
+++ jumoresques-refresher-service/src/main/kotlin/zhi/yest/jumoresquesrefresherservice/config/WebClientConfig.kt
@@ -3,7 +3,10 @@ package zhi.yest.jumoresquesrefresherservice.config
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
+import java.util.logging.Logger
 
 @Configuration
 class WebClientConfig {
@@ -11,5 +14,17 @@ class WebClientConfig {
     @Bean
     fun webClientBuilder(): WebClient.Builder {
         return WebClient.builder()
+                .filter(loggingFunction)
+    }
+
+    private val loggingFunction = ExchangeFilterFunction.ofRequestProcessor {
+        LOG.info("""Sending a ${it.method()} request to ${it.url()}:
+            |   ${it.body()}
+        """.trimMargin())
+        Mono.just(it)
+    }
+
+    companion object {
+        val LOG: Logger = Logger.getLogger("RequestLogger")
     }
 }
